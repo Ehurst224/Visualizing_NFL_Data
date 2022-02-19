2f4ed3501d8f8683a44356a8a369726875243c77
oh-be <83245307+oh-be@users.noreply.github.com>
2022-02-19 14:01:07 -0600

    update

---
+++ app.py
@@ -0,0 +1,76 @@
+import os
+import pandas as pd
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, engine, func, or_
+from sqlalchemy.ext.automap import automap_base
+
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    redirect)
+
+
+engine = create_engine("sqlite:///league.db")
+
+Base = automap_base()
+
+Base.prepare(engine, reflect=True)
+
+print(Base.classes.keys())
+
+nfl = Base.classes.nfl
+qb = Base.classes.qb
+rb = Base.classes.rb
+te = Base.classes.te
+wr = Base.classes.wr
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+
+    return render_template('index.html')
+
+#################################################
+
+@app.route("/data")
+def data():
+
+    return render_template("data.html")
+
+#################################################
+
+@app.route("/player")
+def player():
+
+    return render_template("player.html")
+
+#################################################
+
+@app.route("/team")
+def team():
+
+    return render_template("team.html")
+
+#################################################
+
+@app.route("/position")
+def position():
+
+    return render_template("position.html")
+
+#################################################
+#API
+
+@app.route("/api/nfl")
+def nfl_grid():
+
+    df = pd.read_sql_table('qb',con=engine)
+    table_results = df.to_dict('records')
+    print(df)
+    return jsonify(table_results)
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,4 +1,28 @@
 // read in data from website
-d3.csv("/../../EDA/qb.csv", function(data) {
-    console.log(data)}
-);
+const url = "http://127.0.0.1:5000/api/nfl";
+
+d3.json(url).then(function(data) {
+    console.log(data);
+    let players = data.map(i => i.Player);
+    console.log(players);
+});
+
+
+// // );
+// // d3.json("api/nfl").then((data) => {
+
+// //     console.log(data)
+  
+// //     // $(document).ready(function() {
+// //     $('#example').DataTable( {
+// //         data: data['table'],
+// //         columns: [
+// //             { title: "Player" },
+// //             { title: "Position" },
+// //         ]
+// //     } );
+  
+// //   })
+// d3.json("api/nfl").then((data) => {
+//     console.log(data)
+// });
